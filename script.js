a9b6c0e94502626c263000c6c10828d111bd9d7c
Rustam Isaev <isaev.rustam.a@gmail.com>
2022-05-30 01:00:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const areBracketsBalanced = (str) => {
+  const strAr = str.split("");
+  const arLength = strAr.length;
+
+  if (arLength === 0) return true;
+  if (arLength % 2 !== 0) return false;
+
+  const map = {")":"("};
+  const stack = [];
+
+  for (let i = 0; i < arLength; i++) {
+    const item = strAr[i];
+
+    if ( item === "(" ) stack.push(item);
+
+    if ( item === ")" ) {
+      const lastItem = stack.pop();
+
+      if ( lastItem !== map[")"])  {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+areBracketsBalanced("()()(())") //true
